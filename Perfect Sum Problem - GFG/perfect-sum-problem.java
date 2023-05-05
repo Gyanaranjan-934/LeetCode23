3e73714912e824c16d86bc6b59eb48bc36b4d482
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-06 00:25:40 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -30,10 +30,31 @@ class Solution{
 	public int perfectSum(int arr[],int n, int tar) 
 	{ 
 	    // 1. Recursion + Tabulation
-	    int[][] dp = new int[n][tar+1];
-	    for(int[] row : dp)
-	        Arrays.fill(row,-1);
-	    return recFunc(n-1,arr,tar,dp);
+	   // int[][] dp = new int[n][tar+1];
+	   // for(int[] row : dp)
+	   //     Arrays.fill(row,-1);
+	   // return recFunc(n-1,arr,tar,dp);
+	   
+	   //// 2. Tabulation
+	   int[][] dp = new int[n+1][tar+1];
+	   for(int j=1;j<=tar;j++)
+	        dp[0][j] = 0;
+	    dp[0][0] = 1;
+	    
+	    // tabulation
+	    for(int i=1;i<=n;i++){
+	        for(int j=0;j<=tar;j++){
+	            if(arr[i-1] > j){
+	                dp[i][j] = dp[i-1][j];
+	            }
+	            else{
+	                dp[i][j] = (dp[i-1][j-arr[i-1]] + dp[i-1][j])%1000000007;
+	            }
+	        }
+	    }
+	    
+	    return dp[n][tar];
+	    
 	} 
 	
 	public int recFunc(int ind,int[] arr,int tar,int[][] dp){
