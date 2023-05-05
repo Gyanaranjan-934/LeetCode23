724bc0706808da17d56350679944a877dbb30698
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-06 00:13:15 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int sum = sc.nextInt();
+                    int arr[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                    arr[i] = sc.nextInt();
+                    
+                    Solution ob = new Solution();
+                    System.out.println(ob.perfectSum(arr,n,sum));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+class Solution{
+
+	public int perfectSum(int arr[],int n, int tar) 
+	{ 
+	    // 1. Recursion + Tabulation
+	    int[][] dp = new int[n][tar+1];
+	    for(int[] row : dp)
+	        Arrays.fill(row,-1);
+	    return recFunc(n-1,arr,tar,dp);
+	} 
+	
+	public int recFunc(int ind,int[] arr,int tar,int[][] dp){
+        if(ind == 0){
+            if(tar == 0 && arr[0] == 0){
+                return 2;
+            }
+            else if(tar == 0 || arr[0] == tar) return 1;
+            return 0;
+        }
+        if(dp[ind][tar] != -1)return dp[ind][tar];
+        int notPick = recFunc(ind-1,arr,tar,dp);
+        int pick = 0;
+        if(arr[ind]<=tar)
+            pick = recFunc(ind-1,arr,tar-arr[ind],dp);
+       
+        return dp[ind][tar] = (pick%1000000007+notPick%1000000007)%1000000007;
+	    
+	}
+}
