4d731100c4d272d0309d7e3fdbd2f74713da3e31
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-11 02:14:13 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.java
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.java
@@ -25,11 +25,72 @@ class Solution
 {
     int wildCard(String p, String s)
     {
-        int[][] dp = new int[s.length()][p.length()];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        // 1. Recursion + Memoization
+        // int[][] dp = new int[s.length()][p.length()];
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // return recFunc(s.length()-1,p.length()-1,s,p,dp) ? 1 : 0;
+
+        // 2. Tabulation
+        // int n = s.length();
+        // int m = p.length();
+        // boolean[][] dp = new boolean[n+1][m+1];
+        // dp[0][0] = true;
+        // for(int i=1;i<=n;i++){
+        //     dp[i][0] = false;
+        // }
+        // for(int i=1;i<=m;i++){
+        //     dp[0][i] = isAllStars(p,i-1);
+        // }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s.charAt(i-1) == p.charAt(j-1) ||p.charAt(j-1) =='?'){
+        //             dp[i][j] = dp[i-1][j-1];
+        //         }
+        //         else{
+        //             if(p.charAt(j-1) =='*'){
+        //                 dp[i][j] = dp[i][j-1] || dp[i-1][j];
+        //             }else
+        //                 dp[i][j] = false;
+        //         }
+        //     }
+        // }
+        
+        // return dp[n][m] ? 1 : 0;
+
+        // 2. Space Optimization
+        int n = s.length();
+        int m = p.length();
+        
+        boolean[] prev = new boolean[m+1];
+        boolean[] curr = new boolean[m+1];
+
+        prev[0] = true;
+        
+        for(int i=1;i<=m;i++){
+            prev[i] = isAllStars(p,i-1);
         }
-        return recFunc(s.length()-1,p.length()-1,s,p,dp) ? 1 : 0;
+
+        for(int i=1;i<=n;i++){
+            curr[0] = false;
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1) == p.charAt(j-1) ||p.charAt(j-1) =='?'){
+                    curr[j] = prev[j-1];
+                }
+                else{
+                    if(p.charAt(j-1) =='*'){
+                        curr[j] = curr[j-1] || prev[j];
+                    }else
+                        curr[j] = false;
+                }
+                
+            }
+            prev = (boolean[]) curr.clone();
+            
+        }
+        return prev[m] ? 1 : 0;
     }
     public boolean isAllStars(String p,int j){
         for(int i=j;i>=0;i--){
