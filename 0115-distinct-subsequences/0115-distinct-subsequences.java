43e5dcecd01a564ceea504c6ffe1eb77d62d7afb
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-10 17:49:09 +0530

    Time: 17 ms (80.69%), Space: 40.9 MB (90.07%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -24,20 +24,33 @@ class Solution {
         // }
         // return dp[n][m];
 
-        // 3. Space Optimization
+        // 3. Space Optimization 1
+        // int[] prev = new int[m+1];
+        // int[] curr = new int[m+1];
+        // prev[0] = 1;
+        // curr[0] = 1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=m;j>=1;j--){
+        //         if(s1.charAt(i-1)==s2.charAt(j-1)){
+        //             curr[j] = prev[j-1]+prev[j];
+        //         }else{
+        //             curr[j] = prev[j];
+        //         }
+        //     }
+        //     prev = curr;
+        // }
+
+        // return prev[m];
+
+        // 3. Space Optimization 2
         int[] prev = new int[m+1];
-        int[] curr = new int[m+1];
         prev[0] = 1;
-        curr[0] = 1;
         for(int i=1;i<=n;i++){
             for(int j=m;j>=1;j--){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    curr[j] = prev[j-1]+prev[j];
-                }else{
-                    curr[j] = prev[j];
+                    prev[j] = prev[j-1]+prev[j];
                 }
             }
-            prev = curr;
         }
 
         return prev[m];
