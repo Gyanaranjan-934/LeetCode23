39117c0b4ab65e99f1153feeefa0d78e3fa4cc0b
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-10 17:45:24 +0530

    Time: 16 ms (84.1%), Space: 40.5 MB (94.82%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -6,21 +6,41 @@ class Solution {
         // for(int[] row:dp)
         //     Arrays.fill(row,-1);
         // return recFunc(n-1,m-1,s1,s2,dp);
+
+
         // 2. Tabulation
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
+        // int[][] dp = new int[n+1][m+1];
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = 1;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s1.charAt(i-1)==s2.charAt(j-1)){
+        //             dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+        //         }else{
+        //             dp[i][j] = dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
+
+        // 3. Space Optimization
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
+        prev[0] = 1;
+        curr[0] = 1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                    curr[j] = prev[j-1]+prev[j];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+
+        return prev[m];
 
     }
     public int recFunc(int ind1,int ind2,String s1, String s2,int[][] dp){
