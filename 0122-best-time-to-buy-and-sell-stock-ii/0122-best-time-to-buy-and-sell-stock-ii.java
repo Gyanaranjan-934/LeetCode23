0d97e5a91aac13f89270022c667a0524303f4e62
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-11 13:04:15 +0530

    Time: 160 ms (7.11%), Space: 44.4 MB (6.62%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,33 @@
 class Solution {
     public int maxProfit(int[] arr) {
-        int[][] dp = new int[arr.length+1][2];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
+        // 1. Recursion + Memoization
+        // int[][] dp = new int[arr.length+1][2];
+        // for(int[] row : dp)
+        //     Arrays.fill(row,-1);
         
-        return recFunc(0,arr,1,dp);
+        // return recFunc(0,arr,1,dp);
+
+        // 2. Tabulation
+        int[][] dp = new int[arr.length+1][2];
+        
+        for(int i=arr.length-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(j==1){
+                    int buyIt = -arr[i]+dp[i+1][0];
+                    int notBuyIt = dp[i+1][1];
+                    dp[i][j] = Math.max(buyIt,notBuyIt);
+                }else{
+                    int sellIt = arr[i]+dp[i+1][1];
+                    int notSellIt = dp[i+1][0];
+                    dp[i][j] = Math.max(sellIt,notSellIt);
+                }
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return dp[0][1];
+        
+
     }
     public int recFunc(int ind,int[] arr,int buy,int[][] dp){
         if(ind==arr.length)return 0;
