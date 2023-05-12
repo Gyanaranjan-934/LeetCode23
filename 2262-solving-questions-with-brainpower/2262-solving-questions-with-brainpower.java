8db2fe826004e996ffafdc28e9b0cf6df6ad3686
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-12 11:27:10 +0530

    Time: 7 ms (63.49%), Space: 98.5 MB (54.5%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,11 +1,21 @@
 class Solution {
     public long mostPoints(int[][] mat) {
         int n = mat.length;
-        long[] dp = new long[n+mat[n-1][1]];
-        for(int i=0;i<dp.length;i++){
-            dp[i] = -1;
+        // long[] dp = new long[n+mat[n-1][1]];
+        // for(int i=0;i<dp.length;i++){
+        //     dp[i] = -1;
+        // }
+        // return recFunc(0,mat,dp);
+
+        long[] dp = new long[n+mat[n-1][1]+1];
+        for(int i=n-1;i>=0;i--){
+            int pickInd = Math.min(n,i+mat[i][1]+1);
+            long pick = mat[i][0]+dp[pickInd];
+            long notPick = dp[i+1];
+            dp[i] = Math.max(pick,notPick);
         }
-        return recFunc(0,mat,dp);
+
+        return dp[0];
     }
     public long recFunc(int ind,int[][] mat,long[] dp){
         if(ind>=mat.length)return 0;
