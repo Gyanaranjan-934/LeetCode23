34d689d923e9c53bf8bf6534a07ae824edacacf5
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-12 18:28:00 +0530

    Time: 6 ms (85.45%), Space: 42.6 MB (33.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -23,22 +23,65 @@ class Solution {
         // return dp[0][0];
 
         // Space Optimization
-        int n = nums.length;
-        int[] ahead = new int[n+1];
-        int[] curr = new int[n+1];
+        // int n = nums.length;
+        // int[] ahead = new int[n+1];
+        // int[] curr = new int[n+1];
 
-        for(int ind = n-1;ind>=0;ind--){
-            for(int prev=ind-1;prev>=-1;prev--){
-                int pick = 0;
-                if(prev==-1||nums[ind]>nums[prev]){
-                    pick = 1 + ahead[ind+1];
-                }
-                int notPick = ahead[prev+1];
-                curr[prev+1] = Math.max(pick,notPick);
+        // for(int ind = n-1;ind>=0;ind--){
+        //     for(int prev=ind-1;prev>=-1;prev--){
+        //         int pick = 0;
+        //         if(prev==-1||nums[ind]>nums[prev]){
+        //             pick = 1 + ahead[ind+1];
+        //         }
+        //         int notPick = ahead[prev+1];
+        //         curr[prev+1] = Math.max(pick,notPick);
+        //     }
+        //     ahead = curr;
+        // }
+        // return ahead[0];
+        
+        // Space Optimization T=O(N^2) S=O(N)
+        // int[] dp = new int[size];
+        // Arrays.fill(dp,1);
+        // int maxi = Integer.MIN_VALUE;
+        // for(int i=1;i<size;i++){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i])
+        //             dp[i] = Math.max(dp[i],dp[j]+1);
+        //     }
+        //     maxi = Math.max(dp[i],maxi);
+        // }
+        // return maxi;
+        
+        // Using Binary Search T=O(NLogN) S=O(logN)
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        int size = nums.length;
+        for(int i=1;i<size;i++){
+            if(nums[i]>list.get(list.size()-1)){
+                list.add(nums[i]);
+            }else{
+                // find the index of next greater element
+                int ind = binary_search(list,nums[i]);
+                list.set(ind,nums[i]);
             }
-            ahead = curr;
         }
-        return ahead[0];
+        
+        return list.size();
+        
+    }
+    public int binary_search(ArrayList<Integer> list,int k){
+        int lo = 0,hi = list.size()-1;
+        while(lo<=hi){
+            int mid = (hi+lo)/2;
+            if(list.get(mid)==k)return mid;
+            else if(list.get(mid) > k)
+                hi = mid-1;
+            else
+                lo = mid+1;
+        }
+        return lo;
     }
     public int recFunc(int ind,int[] arr,int prev,int[][] dp){
         if(ind==arr.length){
