e4040c5fd53afb3d96b10d218d5bf23642306908
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-12 13:10:10 +0530

    Time: 116 ms (16.85%), Space: 42.3 MB (52.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,10 +1,44 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        // 1. Recursion + Memoization
+        // int n = nums.length;
+        // int[][] dp = new int[n][n+1];
+        // for(int[] row : dp)
+        //     Arrays.fill(row,-1);
+        // return recFunc(0,nums,-1,dp);
+
+        // 2. Tabulation
+        // int n = nums.length;
+        // int[][] dp = new int[n+1][n+1];
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int prev=ind-1;prev>=-1;prev--){
+        //         int pick = 0;
+        //         if(prev == -1 || nums[ind]>nums[prev]){
+        //             pick = 1 + dp[ind+1][ind+1];
+        //         }
+        //         int notPick = dp[ind+1][prev+1];
+        //         dp[ind][prev+1] = Math.max(pick,notPick);
+        //     }
+        // }
+        // return dp[0][0];
+
+        // Space Optimization
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        return recFunc(0,nums,-1,dp);
+        int[] ahead = new int[n+1];
+        int[] curr = new int[n+1];
+
+        for(int ind = n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int pick = 0;
+                if(prev==-1||nums[ind]>nums[prev]){
+                    pick = 1 + ahead[ind+1];
+                }
+                int notPick = ahead[prev+1];
+                curr[prev+1] = Math.max(pick,notPick);
+            }
+            ahead = curr;
+        }
+        return ahead[0];
     }
     public int recFunc(int ind,int[] arr,int prev,int[][] dp){
         if(ind==arr.length){
