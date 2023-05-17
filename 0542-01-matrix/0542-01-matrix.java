e11830c927b24241ae8b073ee8a4a73c09c3fb1a
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-17 17:00:29 +0530

    Time: 14 ms (72.25%), Space: 47.1 MB (10.22%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,50 @@
+class Pair{
+    int row;
+    int col;
+    int dist;
+    Pair(int _row,int _col,int _dist){
+        this.row = _row;
+        this.col = _col;
+        this.dist = _dist;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        int[][] visited = new int[n][m];
+        int[][] distance = new int[n][m];
+        
+        Queue<Pair> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    visited[i][j]=1;
+                    q.add(new Pair(i,j,0));
+                }
+            }
+        }
+        
+        int[] drow = {-1,0,1,0};
+        int[] dcol = {0,1,0,-1};
+        
+        while(!q.isEmpty()){
+            Pair p = q.remove();
+            int r = p.row;
+            int c = p.col;
+            int v = p.dist;
+            distance[r][c]=v;
+            for(int i=0;i<4;i++){
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    visited[nrow][ncol]=1;
+                    q.add(new Pair(nrow,ncol,v+1));
+                }
+            }
+            
+        }
+        return distance;
+    }
+}
