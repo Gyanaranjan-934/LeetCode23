b08d9dcec1d97e5fab85bf2143335049df04b72f
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-17 13:13:33 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -29,58 +29,68 @@ class GFG {
         }
     }
 }
-// } Driver Code Ends
+// } Driver Code Endshttps://practice.geeksforgeeks.org/contest/job-a-thon-20-hiring-challenge
 
-class Node {
-    int first;
-    int second;
-    public Node(int first, int second) {
-        this.first = first;
-        this.second = second; 
-    }
-}
+// class Solution {
+//     // Function to detect cycle in an undirected graph.
+//     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+//         int[] parent = new int[V];
+//         Arrays.fill(parent,-1);
+//         boolean[] visited = new boolean[V];
+//         Arrays.fill(visited,false);
+//         for(int i=0;i<V;i++){
+//             if(!visited[i]){
+//                 int k = bfs(i,visited,parent,adj);
+//                 if(k==1)return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public int bfs(int ind,boolean[] visited,int[] parent,ArrayList<ArrayList<Integer>> adj){
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(ind);
+//         visited[ind]=true;
+//         while(!q.isEmpty()){
+//             int k = q.remove();
+//             int pt = parent[k];
+//             for(int i :adj.get(k)){
+//                 if(!visited[i]){
+//                     visited[i]=true;
+//                     parent[i]=k;                
+//                     q.add(i);
+//                 }
+//                 else if(i!=pt)return 1;
+//             }
+            
+//         }
+//         return 0;
+//     }
+// }
 class Solution {
     // Function to detect cycle in an undirected graph.
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean vis[] = new boolean[V];
-        Arrays.fill(vis,false);
-        int parent[] = new int[V];
-        Arrays.fill(parent,-1);  
-        
-        for(int i=0;i<V;i++)
-            if(vis[i]==false) 
-                if(checkForCycle(adj, i,vis, parent)) 
-                    return true;
-    
-        return false;
+    private boolean dfs(int node, int parent, int vis[], ArrayList<ArrayList<Integer>> 
+    adj) {
+        vis[node] = 1; 
+        // go to all adjacent nodes
+        for(int adjacentNode: adj.get(node)) {
+            if(vis[adjacentNode]==0) {
+                if(dfs(adjacentNode, node, vis, adj) == true) 
+                    return true; 
+            }
+            // if adjacent node is visited and is not its own parent node
+            else if(adjacentNode != parent) return true; 
+        }
+        return false; 
     }
-    public boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, int s,boolean vis[], int parent[]){
-       Queue<Node> q =  new LinkedList<>(); //BFS
-       q.add(new Node(s, -1));
-       vis[s] =true;
-       
-       // until the queue is empty
-       while(!q.isEmpty())
-       {
-           // source node and its parent node
-           int node = q.peek().first;
-           int par = q.peek().second;
-           q.remove(); 
-           
-           // go to all the adjacent nodes
-           for(Integer it: adj.get(node))
-           {
-               if(vis[it]==false)  
-               {
-                   q.add(new Node(it, node));
-                   vis[it] = true; 
-               }
-        
-                // if adjacent node is visited and is not its own parent node
-               else if(par != it) return true;
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+       int vis[] = new int[V]; 
+       for(int i = 0;i<V;i++) {
+           if(vis[i] == 0) {
+               if(dfs(i, -1, vis, adj) == true) return true; 
            }
        }
-       
-       return false;
+       return false; 
     }
 }
+
