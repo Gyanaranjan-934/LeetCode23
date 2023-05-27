2566585bb2186ac8442ddbe0b156b9bbf01357ba
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-05-27 12:05:01 +0530

    Time: 8 ms (97.49%), Space: 55.9 MB (43.72%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.java
+++ 1522-stone-game-iii/1522-stone-game-iii.java
@@ -11,10 +11,26 @@ class Solution {
         }
         return dp[ind] = diff;
     }
+    
     public String stoneGameIII(int[] arr) {
-        int[] dp = new int[arr.length];
-        Arrays.fill(dp,-1); 
-        int res = recFunc(0,arr,dp);
+        // int[] dp = new int[arr.length];
+        // Arrays.fill(dp,-1); 
+        // int res = recFunc(0,arr,dp);
+
+        // Tabulation
+        int[] dp = new int[arr.length+1];
+        
+        for(int ind=arr.length-1;ind>=0;ind--){
+            int diff = Integer.MIN_VALUE;
+            int currSum=0;
+            for(int i=0;i<3 && ind+i<arr.length;i++){
+                currSum += arr[i+ind];
+                int res = dp[ind+i+1];
+                diff = Math.max(diff,currSum-res);
+            }
+            dp[ind]=diff;
+        }
+        int res = dp[0];
         if(res>0)return "Alice";
         else if(res<0)return "Bob";
         else return "Tie";
