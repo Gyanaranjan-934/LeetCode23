b7371ea05d7e5e34c8861b0962b1f001d468c210
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-07 23:40:12 +0530

    Time: 32 ms (42.35%), Space: 44.4 MB (32.58%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,32 @@
+class Pair{
+    int task;
+    int time;
+    Pair(int a,int b){
+        this.task = a;
+        this.time = b;
+    }
+}
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for(char c:tasks){
+            freq[c-'A']++;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->y-x);
+        for(int i:freq){
+            if(i!=0)pq.add(i);
+        }
+        Queue<Pair> q = new LinkedList<>();
+        int time = 0;
+        while(!pq.isEmpty() || !q.isEmpty()){
+            if(!q.isEmpty() && time==q.peek().time)pq.add(q.poll().task);
+            int task=0;
+            if(!pq.isEmpty())task = pq.poll();
+            time++;
+            if(task>1)
+                q.add(new Pair(task-1,time+n));
+        }
+        return time;
+
+    }
+}
