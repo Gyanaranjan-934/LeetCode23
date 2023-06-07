6444b150d63064ea70711bc1e8b9fa65ccfe031a
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-08 00:00:51 +0530

    Time: 3 ms (69.9%), Space: 45 MB (13.21%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -9,24 +9,30 @@ class Pair{
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
+        int maxFreq = 0;
         for(char c:tasks){
             freq[c-'A']++;
+            maxFreq = Math.max(maxFreq,freq[c-'A']);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->y-x);
-        for(int i:freq){
-            if(i!=0)pq.add(i);
-        }
-        Queue<Pair> q = new LinkedList<>();
-        int time = 0;
-        while(!pq.isEmpty() || !q.isEmpty()){
-            if(!q.isEmpty() && time==q.peek().time)pq.add(q.poll().task);
-            int task=0;
-            if(!pq.isEmpty())task = pq.poll();
-            time++;
-            if(task>1)
-                q.add(new Pair(task-1,time+n));
-        }
-        return time;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((x,y)->y-x);
+        // for(int i:freq){
+        //     if(i!=0)pq.add(i);
+        // }
+        // Queue<Pair> q = new LinkedList<>();
+        // int time = 0;
+        // while(!pq.isEmpty() || !q.isEmpty()){
+        //     if(!q.isEmpty() && time==q.peek().time)pq.add(q.poll().task);
+        //     int task=0;
+        //     if(!pq.isEmpty())task = pq.poll();
+        //     time++;
+        //     if(task>1)
+        //         q.add(new Pair(task-1,time+n));
+        // }
+        // return time;
+        int maxFreqCnt = 0;
+        for(int i:freq)if(i==maxFreq)maxFreqCnt++;
 
+        int time = (maxFreq-1)*(n+1)+maxFreqCnt;
+        return Math.max(time,tasks.length);
     }
 }
