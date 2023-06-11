66c713c096c6db36a4256a3408532276ad3804cf
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-11 12:15:03 +0530

    Time: 64 ms (76.43%), Space: 66.1 MB (97.29%) - LeetHub

--- 1249-snapshot-array/1249-snapshot-array.java
+++ 1249-snapshot-array/1249-snapshot-array.java
@@ -1,29 +1,52 @@
 class SnapshotArray {
-    private int id;
-    private Map<Integer, Map<Integer, Integer>> map;
+    
+    private int snaps = 0;
+    private List<Value>[] lists;
 
     public SnapshotArray(int length) {
-        this.id = 0;
-        this.map = new HashMap<>();
-        
-        for (int i = 0; i < length; i++) {
-            this.map.computeIfAbsent(i, v -> new HashMap<>()).put(id, 0);
+        lists = new List[length];
+        for (int i = 0; i < length; ++i) {
+            lists[i] = new ArrayList<>();
+            lists[i].add(new Value(0, snaps));
         }
     }
     
-    public void set(int index, int val) {
-        map.get(index).put(id, val);
+    public void set(int index, int value) {
+        var list = lists[index];
+        var last = list.get(list.size() - 1);
+        if (last.snap_id == snaps)
+            last.x = value;
+        else
+            list.add(new Value(value, snaps));
     }
     
     public int snap() {
-        return id++;
+        return snaps++;
     }
     
     public int get(int index, int snap_id) {
-        Map<Integer, Integer> subMap = map.get(index);
-        
-        while (!subMap.containsKey(snap_id)) snap_id--;
-        
-        return subMap.get(snap_id);
+        var list = lists[index];
+        int left = 0, right = list.size() - 1;
+        while (left + 1 < right) {
+            int mid = left + ((right - left) >> 1);
+            if (list.get(mid).snap_id < snap_id)
+                left = mid;
+            else if (list.get(mid).snap_id > snap_id)
+                right = mid - 1;
+            else 
+                return list.get(mid).x;
+        }
+        if (list.get(right).snap_id <= snap_id)
+            return list.get(right).x;
+        return list.get(left).x;
+    }
+}
+
+class Value {
+    int x;
+    final int snap_id;
+    Value(int _x, int _s) {
+        x = _x;
+        snap_id = _s;
     }
 }
