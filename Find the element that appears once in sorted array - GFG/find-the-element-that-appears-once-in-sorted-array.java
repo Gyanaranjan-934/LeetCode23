01a43209b7698912011585565c0ae82851c5344d
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-14 13:19:37 +0530

    Added solution - LeetHub

--- Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.java
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.java
@@ -33,36 +33,23 @@ class Solution
 {
     int findOnce(int arr[], int n)
     {
-        if(arr.length ==1 )return  arr[0];
-        int low = 0,high=arr.length-1;
-        while(low <= high)
-        {
-            if(low == high)return arr[low];
-            int mid = low + (high-low)/2;
-            
-            if(arr[mid]!=arr[mid+1] && arr[mid]!=arr[mid-1])return arr[mid];
-            
-            if(arr[mid]==arr[mid-1])
-            {
-                int element_left = (mid-1);
-                if(element_left % 2 != 0){
-                    high = mid - 2;
-                }
-                else{
-                    low = mid+1;    
-                }
+           
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if( mid>0 && mid<n-1 && arr[mid]!=arr[mid+1] && arr[mid]!=arr[mid-1])return arr[mid];
+            // check if the left part has even number of elements;
+            if(mid%2==1){
+                // check if the sorted part contains the single elemet
+                if(mid>0 && arr[mid]==arr[mid-1])low=mid+1;
+                else high=mid-1;
             }
             else{
-                int element_right = (high-mid)+1;
-                if(element_right % 2 != 0){
-                    low = mid + 2;
-                    
-                }
-                else{
-                    high = mid-1;    
-                }
+                if(mid<n-1 && arr[mid]==arr[mid+1])low=mid+1;
+                else high=mid-1;
             }
         }
-        return -1;
+        return arr[low];
     }
 }
