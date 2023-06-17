9f1c12400a0e1ea8235f39a36d7e7a581e25a7de
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-17 23:30:23 +0530

    Time: 1 ms (95.15%), Space: 44.9 MB (7.97%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,76 +1,36 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        // 1. Recursion + Memoization
-        // int[][] dp = new int[arr.length+1][2];
-        // for(int[] row : dp)
-        //     Arrays.fill(row,-1);
-        
-        // return recFunc(0,arr,1,dp);
-
-        // 2. Tabulation
-        // int[][] dp = new int[arr.length+1][2];
-        
-        // for(int i=arr.length-1;i>=0;i--){
-        //     for(int j=0;j<=1;j++){
-        //         if(j==1){
-        //             int buyIt = -arr[i]+dp[i+1][0];
-        //             int notBuyIt = dp[i+1][1];
-        //             dp[i][j] = Math.max(buyIt,notBuyIt);
-        //         }else{
-        //             int sellIt = arr[i]+dp[i+1][1];
-        //             int notSellIt = dp[i+1][0];
-        //             dp[i][j] = Math.max(sellIt,notSellIt);
-        //         }                
-        //     }
-        // }
-        // return dp[0][1];
-        
-        // Space Optimization 1
-        // int[] prev = new int[2];
-        // int[] curr = new int[2];
-        // for(int i=arr.length-1;i>=0;i--){
-        //     for(int j=0;j<=1;j++){
-        //         if(j==1){
-        //             int buyIt = -arr[i]+prev[0];
-        //             int notBuyIt = prev[1];
-        //             curr[j] = Math.max(buyIt,notBuyIt);
-        //         }else{
-        //             int sellIt = arr[i]+prev[1];
-        //             int notSellIt = prev[0];
-        //             curr[j] = Math.max(sellIt,notSellIt);
-        //         }
-        //     }
-        //     prev = (int[])(curr.clone());
-        // }
-        // return prev[1];
-
-        // Space Optimization 2
-
-        int aheadNotBuy = 0 , aheadBuy=0;
-        int currNotBuy = 0,currBuy = 0;
-        for(int i=arr.length-1;i>=0;i--){
-
-            currNotBuy = Math.max(arr[i]+aheadBuy,aheadNotBuy);
-            
-            currBuy = Math.max(-arr[i]+aheadNotBuy,aheadBuy);  
-
-            aheadBuy = currBuy;
-            aheadNotBuy=currNotBuy;
-        }
-        return aheadBuy;
-
-    }
-    public int recFunc(int ind,int[] arr,int buy,int[][] dp){
-        if(ind==arr.length)return 0;
-        if(dp[ind][buy] != -1)return dp[ind][buy];
-        if(buy==1){
-            int buyIt = -arr[ind]+recFunc(ind+1,arr,0,dp);
-            int notBuyIt = recFunc(ind+1,arr,1,dp);
-            return dp[ind][buy] = Math.max(buyIt,notBuyIt);
+    private int recFunc(int ind,int[] prices,int[][] dp,int buyStat){
+        if(ind==prices.length)return 0;
+        if(buyStat==1){
+            int buy = -prices[ind]+recFunc(ind+1,prices,dp,0);
+            int notBuy = recFunc(ind+1,prices,dp,1);
+            return dp[ind][buyStat]=Math.max(buy,notBuy);
         }else{
-            int sellIt = arr[ind]+recFunc(ind+1,arr,1,dp);
-            int notSellIt = recFunc(ind+1,arr,0,dp);
-            return dp[ind][buy] = Math.max(sellIt,notSellIt);
+            int sell = prices[ind]+recFunc(ind+1,prices,dp,1);
+            int notSell = recFunc(ind+1,prices,dp,0);
+            return dp[ind][buyStat]=Math.max(sell,notSell);
         }
+    }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        // int[][] dp = new int[n+1][2];
+        // for(int[] row:dp)Arrays.fill(row,-1);
+        // return recFunc(0,prices,dp,1);
+
+        int[][] dp = new int[n+1][2];
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buyStat=1;buyStat>=0;buyStat--){
+                if(buyStat==1){
+                    int buy = -prices[ind]+dp[ind+1][0];
+                    int notBuy = dp[ind+1][1];
+                    dp[ind][buyStat]=Math.max(buy,notBuy);
+                }else{
+                    int sell = prices[ind]+dp[ind+1][1];
+                    int notSell = dp[ind+1][0];
+                    dp[ind][buyStat]=Math.max(sell,notSell);
+                }
+            }
+        }
+        return dp[0][1];
     }
 }
