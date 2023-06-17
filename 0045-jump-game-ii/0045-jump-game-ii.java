73e0afba28c3e4d12b047898195e4ed553527353
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-18 00:03:05 +0530

    Time: 159 ms (9.96%), Space: 44.7 MB (30.76%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int recFunc(int ind,int[] arr,int[] dp){
+        if(ind>=arr.length-1)return 0;
+        if(dp[ind]!=-1)return dp[ind];
+        int res = (int)1e7;
+        for(int i=1;i<=arr[ind];i++){
+            int temp = 1+recFunc(ind+i,arr,dp);
+            res = Math.min(res,temp);
+        }
+        return dp[ind] = res;
+    }
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return recFunc(0,nums,dp);
+    }
+}
