f3e8be8801e52ec8113d5a570fbcd5e99f9c90ef
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-24 23:42:48 +0530

    Time: 2 ms (100%), Space: 40.2 MB (50%) - LeetHub

--- 2850-construct-the-longest-new-string/2850-construct-the-longest-new-string.java
+++ 2850-construct-the-longest-new-string/2850-construct-the-longest-new-string.java
@@ -1,12 +1,14 @@
 class Solution {
     public int longestString(int x, int y, int z) {
-        int xy = Math.min(x,y)*4;
-        int newx = x-Math.min(x,y);
-        int newy = y-Math.min(x,y);
+        // int xy = Math.min(x,y)*4;
+        // int newx = x-Math.min(x,y);
+        // int newy = y-Math.min(x,y);
 
-        xy += z*2;
-        if(newx>0)xy+=2;
-        else if(newy>0)xy+=2;
-        return xy;
+        // xy += z*2;
+        // if(newx>0)xy+=2;
+        // else if(newy>0)xy+=2;
+        // return xy;
+
+        return (x-(Math.min(x,y))>0 ? 2 : 0) + (Math.min(x,y)*4) + (z*2) + (y-(Math.min(x,y))>0 ? 2 : 0);
     }
 }
