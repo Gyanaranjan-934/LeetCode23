177969e4bbfb5aee4b00f5118de2d2108a3dd98c
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-06-25 15:03:30 +0530

    Time: 38 ms (50%), Space: 43.4 MB (25%) - LeetHub

--- 2831-number-of-beautiful-pairs/2831-number-of-beautiful-pairs.java
+++ 2831-number-of-beautiful-pairs/2831-number-of-beautiful-pairs.java
@@ -1,26 +1,30 @@
 class Solution {
     private int firstDig(int num){
-        while(num>10)num=num/10;
+        while(num >= 10) num = num / 10;
         return num;
     }
-    private boolean isCoprime(int a,int b){
-        for(int i=9;i>1;i--){
-            if(a%i== 0 && b%i==0)return false;
+
+    private boolean isCoprime(int a, int b){
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        return true;
+        return a == 1;
     }
+
     public int countBeautifulPairs(int[] nums) {
         int count = 0;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                String s = Integer.toString(nums[i]);
-                int a = s.charAt(0)-'0';
-                int b = nums[j]%10;
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                int a = firstDig(nums[i]);
+                int b = nums[j] % 10;
                 
-                if(isCoprime(a,b))
+                if(isCoprime(a, b))
                     count++;
             }
         }
         return count;
     }
+
 }
