47c544e9d0a4b6015274bb67059975b452c645f0
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-04 13:18:17 +0530

    Time: 4 ms (66.85%), Space: 44.5 MB (11.4%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,9 +1,25 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // brute force hashmap
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i:nums)map.put(i,map.getOrDefault(i,0)+1);
-        for(int i:nums)if(map.get(i)==1)return i;
-        return -1;
+        // brute force hashmap O(N)time O(N)space
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i:nums)map.put(i,map.getOrDefault(i,0)+1);
+        // for(int i:nums)if(map.get(i)==1)return i;
+        // return -1;
+
+        // better solution O(32N) time O(1) space
+        int ans = 0;
+        for(int i=0;i<32;i++){
+            int sum = 0;
+            for(int j=0;j<nums.length;j++){
+                if(((nums[j] >> i) & 1) == 1){
+                    sum++;
+                    sum%=3;
+                }
+            }
+            if(sum != 0){
+                ans |= sum << i;
+            }
+        }
+        return ans;
     }
 }
