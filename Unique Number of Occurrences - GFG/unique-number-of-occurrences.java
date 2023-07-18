7a54261ec0c34b00a803c059f6c4732c1d11dbbe
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-18 11:48:48 +0530

    Added solution - LeetHub

---
+++ Unique Number of Occurrences - GFG/unique-number-of-occurrences.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            boolean res = obj.isFrequencyUnique(n, arr);
+            
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static boolean isFrequencyUnique(int n, int[] arr) {
+        Arrays.sort(arr);
+        int i=0;
+        int j=1;
+        HashSet<Integer> set = new HashSet<>();
+        while(j<n){
+            if(arr[i]!=arr[j]){
+               int freq = j-i;
+               if(set.contains(freq))return false;
+               else{
+                   set.add(freq);
+                   i=j;
+               }
+            }
+            j++;
+        }
+        return !set.contains(j-i);
+        // return true;
+    }
+}
+        
