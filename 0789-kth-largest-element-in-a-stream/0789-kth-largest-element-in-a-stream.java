1f121e99b0c12347d706e81e7f4cfaf66c9d4a15
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-27 01:40:03 +0530

    Time: 14 ms (99.79%), Space: 46.4 MB (93.76%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -1,33 +1,18 @@
 class KthLargest {
-    ArrayList<Integer> list ;
-    int k;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    int k ;
     public KthLargest(int k, int[] nums) {
-        Arrays.sort(nums);
-        list = new ArrayList<>();
-        for(int i:nums)list.add(i);
         this.k = k;
+        for(int i:nums){
+            if(pq.size()<=k)pq.add(i);
+            while(pq.size()>k)pq.remove();
+        }
     }
     
     public int add(int val) {
-        int ind = findInd(list,val);
-        list.add(ind,val);
-        return list.get(list.size()-this.k);
-    }
-    private int findInd(ArrayList<Integer> list,int val){
-        int low = 0;
-        int high = list.size()-1;
-        while(low<=high){
-            int mid = (high+low)/2;
-            if(list.get(mid)==val){
-                return mid;
-            }else if(list.get(mid)>val){
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
-
-        return low ;
+        pq.add(val);
+        while(pq.size()>this.k)pq.remove();
+        return pq.peek();
     }
 }
 
