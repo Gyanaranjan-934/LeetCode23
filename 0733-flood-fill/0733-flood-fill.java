3380d8e7c9f3ab1e6ab3d729a9cda7ee5e0a567b
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-28 02:18:07 +0530

    Time: 1 ms (49.79%), Space: 43.9 MB (85.33%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,53 +1,46 @@
+import java.util.*;
+
+class Pair {
+    int x;
+    int y;
+
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // int[][] ans = image;
-
-        // int iniCol = image[sr][sc];
-        // int drow[] = {-1, 0, +1, 0};
-        // int dcol[] = {0, +1, 0, -1}; 
-        // dfs(sr,sc,ans,image,iniCol,color,drow,dcol);
-        // return ans;
-
-        int n = image.length, m = image[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        int oldColor = image[sr][sc];
-        image[sr][sc] = color;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int originalColor = image[sr][sc];
+        if (originalColor == newColor) {
+            return image; // No need to perform flood fill if the colors are already the same
+        }
         
-        int[] row = {-1, 1, 0, 0};
-        int[] col = {0, 0, -1, 1};
+        int n = image.length;
+        int m = image[0].length;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
-        q.add(new int[]{sr, sc});
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int[] point = q.poll();
-                for(int j=0;j<4;j++){
-                    int x = point[0] + row[j];
-                    int y = point[1] + col[j];
-                    if(x>=0 && x<n && y >= 0 && y < m && image[x][y] == oldColor && image[x][y] != color){
-                        image[x][y] = color;
-                        q.add(new int[]{x, y});
-                    }
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(sr, sc));
+        image[sr][sc] = newColor; // Change the color of the starting pixel
+        
+        while (!queue.isEmpty()) {
+            Pair currentPixel = queue.poll();
+            int x = currentPixel.x;
+            int y = currentPixel.y;
+            
+            for (int[] dir : directions) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && image[nx][ny] == originalColor) {
+                    queue.add(new Pair(nx, ny));
+                    image[nx][ny] = newColor; // Change the color of the neighboring pixel
                 }
             }
         }
         
         return image;
-
-    }
-    private void dfs(int r,int c,int[][] ans,int[][] image,int iniCol,int newCol,int[] drow,int[] dcol){
-        
-        ans[r][c]=newCol;
-        
-        for(int i=0;i<4;i++){
-            int nrow = r + drow[i];
-            int ncol = c + dcol[i];
-            if(nrow >=0 && nrow <ans.length && ncol>=0 && ncol<ans[0].length && image[nrow][ncol]==iniCol && ans[nrow][ncol]!=newCol){
-
-                dfs(nrow,ncol,ans,image,iniCol,newCol,drow,dcol);
-                
-            }
-
-        }
     }
 }
