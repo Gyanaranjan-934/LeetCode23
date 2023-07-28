9f9e7c925a8e905c81edf7452bbd25e58fc074f4
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-28 11:47:17 +0530

    Time: 0 ms (100%), Space: 39.6 MB (92.26%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,16 +1,18 @@
 class Solution {
-    private int recFunc(int l,int h,int[] nums,int flag){
+    private int recFunc(int l,int h,int[] nums,int[][] dp){
         if(l>h)return 0;
         if(l==h)return nums[l];
+        if(dp[l][h]!=-1)return dp[l][h];
+        int pickL = nums[l] - recFunc(l+1,h,nums,dp);
+        int pickR = nums[h] - recFunc(l,h-1,nums,dp);
 
-        int pickL = nums[l] - recFunc(l+1,h,nums,flag);
-        int pickR = nums[h] - recFunc(l,h-1,nums,flag);
-
-        return Math.max(pickL,pickR);
+        return dp[l][h] = Math.max(pickL,pickR);
 
     }
     public boolean PredictTheWinner(int[] nums) {
-        int res = recFunc(0,nums.length-1,nums,0);
+        int[][] dp = new int[nums.length][nums.length];
+        for(int[] row : dp)Arrays.fill(row,-1);
+        int res = recFunc(0,nums.length-1,nums,dp);
         return res>=0;
     }
 }
