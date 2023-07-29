7d28c9f27fa82f78927b8aa6927082ab4f67e8c4
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-29 19:17:00 +0530

    Time: 347 ms (98.65%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,1 @@
+select a1.machine_id,CAST(ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS FLOAT) as processing_time from Activity as a1 inner join Activity as a2 on a1.machine_id=a2.machine_id and a1.process_id = a2.process_id and a1.activity_type!=a2.activity_type where a1.timestamp < a2.timestamp group by a1.machine_id
