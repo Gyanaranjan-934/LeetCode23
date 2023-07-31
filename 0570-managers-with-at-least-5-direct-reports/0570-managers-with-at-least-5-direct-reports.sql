c19f7c86780dc89a2b9e3e40de61e627217a1145
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-07-31 15:58:38 +0530

    Time: 744 ms (18%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,3 @@
 select e2.name from employee as e1
 join 
-employee as e2 where e1.managerid = e2.id group by e2.id having count(e2.id) > 4
+employee as e2 where e1.managerid = e2.id group by e2.id having count(e2.id)>4
