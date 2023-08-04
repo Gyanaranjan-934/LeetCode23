391c11d947ab7f3284fd6cbb3fe6f2db09ebfed2
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-08-04 16:18:25 +0530

    Time: 21 ms (48.41%), Space: 48.9 MB (17.2%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,31 +1,21 @@
 class Solution {
-    private int findCnt(int[] nums,int goal){
-        if(goal<0)return 0;
-        int low = 0;
-        int high = 0;
-        int n = nums.length;
-        int curr = 0;
-        int res = 0;
-        while(high<n){
-            curr+=nums[high];
-            while(curr>goal){
-                curr-=nums[low++];
-            }
-            res += high-low+1;
-            high++;
-        }
-        return res;
-    }
     public int numSubarraysWithSum(int[] nums, int goal) {
-        // return findCnt(nums,goal)-findCnt(nums,goal-1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
         int cnt = 0;
+        // map.put(0,1);
         for(int i=0;i<nums.length;i++){
-            int sum = 0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==goal)cnt++;
+            sum += nums[i];
+            if(sum==goal)cnt++;
+            if(map.containsKey(sum-goal)){
+                cnt+=map.get(sum-goal);
             }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
+        // if(map.containsKey(sum-goal)){
+        //     cnt+=map.get(sum-goal);
+        // }
+        
         return cnt;
     }
 }
