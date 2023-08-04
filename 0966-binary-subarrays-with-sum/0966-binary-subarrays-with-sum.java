cd776bca1a7f7a447a672fe9d5f2b6928cfabc72
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-08-04 16:15:30 +0530

    Time: 2596 ms (5.02%), Space: 46.7 MB (90.45%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -17,7 +17,15 @@ class Solution {
         return res;
     }
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return findCnt(nums,goal)-findCnt(nums,goal-1);
-
+        // return findCnt(nums,goal)-findCnt(nums,goal-1);
+        int cnt = 0;
+        for(int i=0;i<nums.length;i++){
+            int sum = 0;
+            for(int j=i;j<nums.length;j++){
+                sum+=nums[j];
+                if(sum==goal)cnt++;
+            }
+        }
+        return cnt;
     }
 }
