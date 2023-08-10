ea183840abc20ef433d85789c4093d93f8aee508
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-08-10 22:31:47 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.java
+++ Longest Common Subsequence - GFG/longest-common-subsequence.java
@@ -26,38 +26,21 @@ class GFG {
 
 class Solution
 {
-    //Function to find the length of longest common subsequence in two strings.
-    
-    static int lcs(int x, int y, String s1, String s2)
-    {
-        // 1. Recursion + Memoization
-        // int[][] dp = new int[s1.length()][s2.length()];
-        // for(int[] row : dp)
-        //     Arrays.fill(row,-1);
-        // return recFunc(s1.length()-1,s2.length()-1,s1,s2,dp);
-        
-        // 2. Tabulation
-        int n = s1.length()+1;
-        int m = s2.length()+1;
-        int[][] dp = new int[n][m];
-        
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n-1][m-1];
-
-    }
     static int recFunc(int ind1,int ind2,String s1,String s2,int[][] dp){
         if(ind1<0 || ind2<0)return 0;
-
-        if(dp[ind1][ind2] != -1)return dp[ind1][ind2];
-
-        if(s1.charAt(ind1)==s2.charAt(ind2))return dp[ind1][ind2] = 1 + recFunc(ind1-1,ind2-1,s1,s2,dp);
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        
+        if(s1.charAt(ind1)==s2.charAt(ind2))
+            return dp[ind1][ind2] = 1 + recFunc(ind1-1,ind2-1,s1,s2,dp);
+        
         return dp[ind1][ind2] = Math.max(recFunc(ind1-1,ind2,s1,s2,dp),recFunc(ind1,ind2-1,s1,s2,dp));
     }
+    //Function to find the length of longest common subsequence in two strings.
+    static int lcs(int x, int y, String s1, String s2)
+    {
+        int[][] dp = new int[x+1][y+1];
+        for(int[] row : dp)Arrays.fill(row,-1);
+        return recFunc(x-1,y-1,s1,s2,dp);
+    }
+    
 }
