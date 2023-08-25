c5b61083703fc9244462f7641dcf66db311b1e30
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-08-26 01:53:50 +0530

    Time: 42 ms (77.88%), Space: 44.4 MB (89.57%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -1,25 +1,24 @@
 class Solution {
-    private int countSubarraysAtmostKDistinct(int[] nums,int k){
+    private int findAtMostK(int[] nums, int k){
+        if(k<0)return 0;
         HashMap<Integer,Integer> map = new HashMap<>();
-        int cnt = 0;
         int n = nums.length;
-        int i=0;
+        int i = 0;
         int j = 0;
+        int cnt = 0;
         while(j<n){
             map.put(nums[j],map.getOrDefault(nums[j],0)+1);
             while(map.size()>k){
-                int temp = map.get(nums[i]);
-                if(temp==1)map.remove(nums[i]);
-                else map.put(nums[i],temp-1);
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(nums[i])==0)map.remove(nums[i]);
                 i++;
             }
-            if(map.size()<=k)cnt+=(j-i+1);
+            cnt+=(j-i+1);
             j++;
         }
         return cnt;
     }
     public int subarraysWithKDistinct(int[] nums, int k) {
-        
-        return countSubarraysAtmostKDistinct(nums,k)-countSubarraysAtmostKDistinct(nums,k-1);
+        return findAtMostK(nums,k)-findAtMostK(nums,k-1);
     }
 }
