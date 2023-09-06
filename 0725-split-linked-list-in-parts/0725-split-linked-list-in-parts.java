803965e98db5217e54d8d27696b1a8e72f2217ea
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-09-06 12:34:55 +0530

    Time: 0 ms (100%), Space: 43.5 MB (63.92%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -11,37 +11,24 @@
 class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
         ListNode[] res = new ListNode[k];
-        int size = 0;
         ListNode ptr = head;
+        int size = 0;
         while(ptr!=null){
-            ptr = ptr.next;
+            ptr=ptr.next;
             size++;
         }
         int comp = size/k;
-        int extra = size%k;
-        ptr = head;
+        int extras = size%k;
+        ptr=head;
         ListNode prev = null;
-
-        // Loop through each part.
-        for (int i = 0; ptr != null && i < k; i++, extra--) {
-            // Store the current ptr as the start of the current part.
-            res[i] = ptr;
-
-            // Traverse comp + 1 nodes if there are remaining extra nodes (extra > 0).
-            // Otherwise, traverse only comp nodes.
-            for (int j = 0; j < comp + (extra > 0 ? 1 : 0); j++) {
+        for(int i=0;i<k&&ptr!=null;i++,extras--){
+            res[i]=ptr;
+            for(int j=0;j<comp+(extras>0 ? 1 : 0);j++){
                 prev = ptr;
                 ptr = ptr.next;
             }
-
-            // Disconnect the current part from the rest of the list by setting prev.next to null.
-            if (prev != null) {
-                prev.next = null;
-            }
+            prev.next=null;
         }
-
-        // Return the array of k parts.
         return res;
-
     }
 }
