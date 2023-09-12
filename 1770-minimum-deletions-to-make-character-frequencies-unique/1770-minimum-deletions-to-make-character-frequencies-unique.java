50f65f736c14cf9047d6e46ee44adf2b11de7673
Gyanaranjan Sahoo <93605334+Gyanaranjan-934@users.noreply.github.com>
2023-09-12 10:49:42 +0530

    Time: 64 ms (12.02%), Space: 44.6 MB (37.1%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,36 +1,21 @@
-class Solution {
+public class Solution {
     public int minDeletions(String s) {
-        // Create a map to store character frequencies
-        Map<Character, Integer> charFrequency = new HashMap<>();
-        
-        // Create a set to keep track of existing frequencies in new string
-        Set<Integer> existingFrequencies = new HashSet<>();
-        
-        // Count character frequencies
-        for (char c : s.toCharArray()) {
-            charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
-        }
-
-        // Create a max heap to store frequencies
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-
-        // Push frequencies into the max heap
-        for (int freq : charFrequency.values()) {
-            maxHeap.offer(freq);
-        }
-
+        HashMap<Character, Integer> cnt = new HashMap<>();
         int deletions = 0;
-        while (!maxHeap.isEmpty()) {
-            int curFreq = maxHeap.poll();
-            
-            // Adjust frequency if it already exists in the new string
-            while (curFreq > 0 && existingFrequencies.contains(curFreq)) {
-                curFreq--;
+        HashSet<Integer> used_frequencies = new HashSet<>();
+        
+        for (char c : s.toCharArray()) {
+            cnt.put(c, cnt.getOrDefault(c, 0) + 1);
+        }
+        
+        for (int freq : cnt.values()) {
+            while (freq > 0 && used_frequencies.contains(freq)) {
+                freq--;
                 deletions++;
             }
-            // Add the adjusted frequency to the set
-            existingFrequencies.add(curFreq);
+            used_frequencies.add(freq);
         }
+        
         return deletions;
     }
 }
